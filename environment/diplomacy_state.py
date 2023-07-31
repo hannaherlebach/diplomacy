89e84b0d63f3b609bd83f80f1a98722512d14f51
Hannah Erlebach <hannah.erlebach@gmail.com>
2023-07-31 17:31:00 +0100

    Clean up powers

--- environment/diplomacy_state.py
+++ environment/diplomacy_state.py
@@ -18,6 +18,7 @@
 from typing import Sequence
 import numpy as np
 import typing_extensions
+from collections import OrderedDict
 
 from diplomacy.environment import observation_utils as utils
 from diplomacy.environment import mila_actions
@@ -64,224 +65,230 @@ from diplomacy.welfare-diplomacy.diplomacy.engine.game import Game
 
 class WelfareDiplomacyState(DiplomacyState):
   
-  def __init__(self, game: Game):
-    self.game = game
+    def __init__(self, game: Game):
+        self.game = game
 
-  def is_terminal(self) -> bool:
-    return self.game.is_game_done
-  
-  def observation(self) -> utils.Observation:
-    """ Gets a utils.Observation namedtuple."""
+        # Get a sorted list of power tuples of form 'power name: power instance'
+        # game.powers is a dict of form {'FRANCE': FrancePower, ...}
+        power_names_sorted = game.powers.keys().sorted()
+        self.powers = OrderedDict((name, game.powers[name]) for name in power_names_sorted)
 
-    game = self.game
-
-    # SEASON: utils.Season
-
-      # DeepMind <-> MILA conversions:
-      # ? = NEWYEAR
-      # SPRING_MOVES = SPRING MOVEMENT
-      # SPRING_RETREATS = SPRING RETREATS
-      # AUTUMN_MOVES = FALL MOVEMENT
-      # AUTUMN_RETREATS = FALL RETREATS
-      # BUILDS = WINTER ADJUSTMENT
+    def is_terminal(self) -> bool:
+        return self.game.is_game_done
     
-    # To do: figure out the NEWYEAR phase
+    def observation(self) -> utils.Observation:
+        """ Gets a utils.Observation namedtuple."""
 
-    # game.phase: string containing long rep of current phase
-    if 'SPRING' in game.phase:
-        season = 'SPRING'
-    elif 'FALL' in game.phase:
-        season = 'AUTUMN'
-    elif 'WINTER' in game.phase:
-        return getattr(utils.Season, 'BUILD')
-    else:
-        raise ValueError('not a season')
+        game = self.game
+        powers = self.powers
 
-    # game.phase_type: 'M' for Movement, 'R' for Retreats, 'A' for Adjustment, '-' for non-playing
-    if 'M' in game.phase_type:
-        type = 'MOVES'
-    elif 'R' in game.phase_type:
-        type = 'RETREATS'
-    else:
-        raise ValueError('not a season')
+        # SEASON: utils.Season
 
-    season = getattr(utils.Season, season + '_' + type)
-    
+        # DeepMind <-> MILA conversions:
+        # ? = NEWYEAR
+        # SPRING_MOVES = SPRING MOVEMENT
+        # SPRING_RETREATS = SPRING RETREATS
+        # AUTUMN_MOVES = FALL MOVEMENT
+        # AUTUMN_RETREATS = FALL RETREATS
+        # BUILDS = WINTER ADJUSTMENT
+        
+        # To do: figure out the NEWYEAR phase
 
+        # game.phase: string containing long rep of current phase
+        if 'SPRING' in game.phase:
+            season = 'SPRING'
+        elif 'FALL' in game.phase:
+            season = 'AUTUMN'
+        elif 'WINTER' in game.phase:
+            return getattr(utils.Season, 'BUILD')
+        else:
+            raise ValueError('not a season')
 
-     
-    # BOARD STATE & BUILD NUMBERS
-      # Board: np.array shape (81, 35)
-      # Build numbers: np.array shape (7,)
-    power_names_sorted = game.powers.keys().sorted()
-    powers = [game.powers[name] for name in power_names_sorted]
+        # game.phase_type: 'M' for Movement, 'R' for Retreats, 'A' for Adjustment, '-' for non-playing
+        if 'M' in game.phase_type:
+            type = 'MOVES'
+        elif 'R' in game.phase_type:
+            type = 'RETREATS'
+        else:
+            raise ValueError('not a season')
 
-    supply_centers = game.map.scs # tags
-    supply_centers_owned = set()
+        season = getattr(utils.Season, season + '_' + type)
+        
+        
+        # BOARD STATE & BUILD NUMBERS
+        # Board: np.array shape (81, 35)
+        # Build numbers: np.array shape (7,)
+ 
+        supply_centers = game.map.scs # tags
+        supply_centers_owned = set()
 
-    # Initialise build numbers
-    build_numbers = np.zeros((7,1), dtype=np.uint8)
+        # Initialise build numbers
+        build_numbers = np.zeros((7,1), dtype=np.uint8)
 
-    # Initialise matrix components
-    unit_types = np.zeros((81,3), dtype=np.uint8)
-    unit_owners = np.zeros((81,8), dtype=np.uint8)
-    buildables = np.zeros((81,1), dtype=np.uint8)
-    removables = np.zeros((81,1), dtype=np.uint8)
-    dislodgeds = np.zeros((81,3), dtype=np.uint8)
-    dislodged_owners = np.zeros((81,8), dtype=np.uint8)
-    area_types = np.zeros((81,3), dtype=np.uint8)
-    sc_owners = np.zeros((81,8), dtype=np.uint8)
+        # Initialise matrix components
+        unit_types = np.zeros((81,3), dtype=np.uint8)
+        unit_owners = np.zeros((81,8), dtype=np.uint8)
+        buildables = np.zeros((81,1), dtype=np.uint8)
+        removables = np.zeros((81,1), dtype=np.uint8)
+        dislodgeds = np.zeros((81,3), dtype=np.uint8)
+        dislodged_owners = np.zeros((81,8), dtype=np.uint8)
+        area_types = np.zeros((81,3), dtype=np.uint8)
+        sc_owners = np.zeros((81,8), dtype=np.uint8)
 
-    # Set default to no unit
-    unit_types[:, -1] = 1
-    unit_owners[:, -1] = 1
-    dislodgeds[:, -1] = 1
-    dislodged_owners[:, -1] = 1
+        # Set default to no unit
+        unit_types[:, -1] = 1
+        unit_owners[:, -1] = 1
+        dislodgeds[:, -1] = 1
+        dislodged_owners[:, -1] = 1
 
-    # Areas containing units and owned supply centres
-    for power_ix, power in enumerate(powers):
+        # Areas containing units and owned supply centres
+        for power_ix, (power_name, power) in enumerate(powers.items()):
 
-        build_count = len(power.centers) - len(power.units)
-        self_occupied_homes = set() # for finding buildable homes
+            build_count = len(power.centers) - len(power.units)
+            self_occupied_homes = set() # for finding buildable homes
 
-        for unit in power.units:
-            
-            is_dislodged = bool(unit[0]=="*")
-
-            unit = unit[1:] if is_dislodged else unit
-            unit_type = unit[0]
-            mila_tag = unit[2:]
-            province = mila_tag[:3]
-            id = mila_actions.mila_to_dm_area(mila_tag)
-
-            # Fill in unit & owner info for area
-            unit_type_ix = 0 if unit_type=='A' else 1
-
-            if not is_dislodged:
-                unit_types[id, unit_type_ix] = 1
-                unit_owners[id, power_ix] = 1
-                # Set 'no unit' to 0
-                unit_types[id, -1] = 0
-                unit_owners[id, -1] = 0
-            else:
-                dislodgeds[id, unit_type_ix] = 1
-                dislodged_owners[id, power_ix] = 1
-                # Set 'no dislodged unit' to 0
-                dislodgeds[id, -1] = 0
-                dislodged_owners[id, -1] = 0
-
-            # Check if unit occupies one of power's homes
-            if province in power.homes:
-                self_occupied_homes.add(province)
-
-            # Removable: if power has more units than centers, then this unit is removable
-            if build_count < 0:
-                removables[id] = 1
+            for unit in game.get_units(power_name): # contains dislodgement info (*), unlike power.units
                 
-        buildable_homes = set()
+                is_dislodged = bool(unit[0]=="*")
 
-        for sc in power.centers: # mainland only
+                unit = unit[1:] if is_dislodged else unit
+                unit_type = unit[0]
+                mila_tag = unit[2:]
+                province = mila_tag[:3]
+                id = mila_actions.mila_to_dm_area(mila_tag)
+
+                # Fill in unit & owner info for area
+                unit_type_ix = 0 if unit_type=='A' else 1
+
+                if not is_dislodged:
+                    unit_types[id, unit_type_ix] = 1
+                    unit_owners[id, power_ix] = 1
+                    # Set 'no unit' to 0
+                    unit_types[id, -1] = 0
+                    unit_owners[id, -1] = 0
+                else:
+                    dislodgeds[id, unit_type_ix] = 1
+                    dislodged_owners[id, power_ix] = 1
+                    # Set 'no dislodged unit' to 0
+                    dislodgeds[id, -1] = 0
+                    dislodged_owners[id, -1] = 0
+
+                # Check if unit occupies one of power's homes
+                if province in power.homes:
+                    self_occupied_homes.add(province)
+
+                # Removable: if power has more units than centers, then this unit is removable
+                if build_count < 0:
+                    removables[id] = 1
+                    
+            buildable_homes = set()
+
+            for sc in power.centers: # mainland only
+                id = mila_actions.mila_to_dm_area(sc)
+
+                # Supply center owner
+                sc_owners[id, power_ix] = 1
+
+                # Buildable: if power controls its home province, there's not a unit in it, and power has more SCs than units, then this area is buildable (since the power controls its home supply center, and building only happens in winter, there cannot be a different power's unit in it at build time)
+                if sc in power.homes and sc not in self_occupied_homes and build_count > 0:
+                    buildables[id] = 1
+                    buildable_homes.add(sc)
+
+                    supply_centers_owned.add(sc)
+
+            # Set build numbers if build season
+            if season == 'BUILD':
+                if build_count < 0:
+                    build_numbers[power_ix] = build_count
+                else:
+                    build_numbers[power_ix] = min(build_count, len(buildable_homes))
+        
+        # Store build numbers if build season
+        if season == 'BUILD':          
+            self._build_numbers = build_numbers
+        # Otherwise access numbers from last build season with positives zeroed out, if not first year
+        elif self._build_numbers:
+            mask = self._build_numbers < 0
+            build_numbers = self._build_numbers * mask
+
+        
+        # Unoccupied supply centres
+        supply_centers_unowned = [sc for sc in supply_centers if sc not in supply_centers_owned]
+        for sc in supply_centers_unowned:
             id = mila_actions.mila_to_dm_area(sc)
+            sc_owners[id, -1] = 1
 
-            # Supply center owner
-            sc_owners[id, power_ix] = 1
+        # Area type: fill out from area id
+        for id in range(81):
+            province_id, area_ix = utils.province_id_and_area_index(id)
+            if area_ix==1 or area_ix==2: # coasts of bicoastals
+                area_types[id, 2] = 1
+            elif province_id >= 14 and province_id < 33: # sea
+                area_types[id,1] = 1
+            else: # all land with 0 or 1 coast
+                area_types[id, 0] = 1
 
-            # Buildable: if power controls its home province, there's not a unit in it, and power has more SCs than units, then this area is buildable (since the power controls its home supply center, and building only happens in winter, there cannot be a different power's unit in it at build time)
-            if sc in power.homes and sc not in self_occupied_homes and build_count > 0:
-               buildables[id] = 1
-               buildable_homes.add(sc)
+        board = np.concatenate(
+            (unit_types, unit_owners, buildables, removables, dislodgeds, dislodged_owners, area_types, sc_owners),
+            dim=1)
 
-            supply_centers_owned.add(sc)
+        # LAST ACTIONS
+        # Using the step method
+        if self._last_actions:
+            last_actions = [action for power in self._last_actions for action in power]
+        else:
+            last_actions = None
 
-        # Set build numbers if build season
-        if season == 'BUILD':
-            if build_count < 0:
-                build_numbers[power_ix] = build_count
-            else:
-                build_numbers[power_ix] = min(build_count, len(buildable_homes))
+        return utils.Observation(season, board, build_numbers, last_actions)
     
-    # Store build numbers if build season
-    if season == 'BUILD':          
-      self._build_numbers = build_numbers
-    # Otherwise access numbers from last build season with positives zeroed out, if not first year
-    elif self._build_numbers:
-        mask = self._build_numbers < 0
-        build_numbers = self._build_numbers * mask
+    def legal_actions(self) -> Sequence[Sequence[int]]:
 
+        pass
     
-    # Unoccupied supply centres
-    supply_centers_unowned = [sc for sc in supply_centers if sc not in supply_centers_owned]
-    for sc in supply_centers_unowned:
-       id = mila_actions.mila_to_dm_area(sc)
-       sc_owners[id, -1] = 1
+    def returns(self) -> np.ndarray:
+        """The returns of the game. All 0s if the game is in progress."""
 
-    # Area type: fill out from area id
-    for id in range(81):
-        province_id, area_ix = utils.province_id_and_area_index(id)
-        if area_ix==1 or area_ix==2: # coasts of bicoastals
-            area_types[id, 2] = 1
-        elif province_id >= 14 and province_id < 33: # sea
-            area_types[id,1] = 1
-        else: # all land with 0 or 1 coast
-            area_types[id, 0] = 1
+        # (Not sure if this should be cumulative, or only at end of game)
+        if self.game.get_current_phase() == 'COMPLETED':
+            welfare_points = [power.welfare_points for power in self.powers.values()]
+            return np.array(welfare_points)
+        else:
+            return np.zeros(7) # dtype?
+    
+    def step(self, actions_per_player: Sequence[Sequence[int]]) -> None:
+        
+        game = self.game
+        powers = self.powers
 
-    board = np.concatenate(
-        (unit_types, unit_owners, buildables, removables, dislodgeds, dislodged_owners, area_types, sc_owners),
-        dim=1)
+        # Convert actions to MILA orders; orders will be lists
+        orders = [mila_actions.action_to_mila_actions(act) 
+                for player in actions_per_player 
+                for act in player]
+        
+        orders_reduced = [[],[],[],[],[],[],[],]
 
-    # LAST ACTIONS
-      # Using the step method
-    if self._last_actions:
-      last_actions = [action for power in self._last_actions for action in power]
-    else:
-      last_actions = None
+        # Reduces lists of possible orders to a single order
+        for player_ix, orders_per_player in enumerate(orders):
+            for order in orders_per_player:
+                # order is a list of possible orders
+                if len(order) > 1:
+                    # Get legal one from list of possible actions - uses self.legal_actions
+                    for possible_order in order:
+                        if possible_order in self.legal_actions[player_ix, :]:
+                            orders_reduced[player_ix].append(possible_order)
+                            break
+                else:
+                    # Get string from single-item list
+                    orders_reduced[player_ix].append(order[0])
 
-    return utils.Observation(season, board, build_numbers, last_actions)
-  
-  def returns(self) -> np.ndarray:
-     """The returns of the game. All 0s if the game is in progress."""
+        orders = orders_reduced
 
-     # (Not sure if this should be cumulative, or only at end of game)
-     if self.game.get_current_phase() == 'COMPLETED':
-        welfare_points = [power.welfare_points for power in self.game.powers]
-        return np.array(welfare_points)
-     else:
-        return np.zeros(7) # dtype?
-  
-  def step(self, actions_per_player: Sequence[Sequence[int]]) -> None:
-     
-     game = self.game
-     power_names_sorted = game.powers.keys().sorted()
+        # Set orders for each power
+        for power_ix, power_name in enumerate(powers.keys()):
+            game.set_orders(power_name, orders[power_ix], expand=False)
 
-     # Convert actions to MILA orders; orders will be lists
-     orders = [mila_actions.action_to_mila_actions(act) 
-               for player in actions_per_player 
-               for act in player]
-     
-     orders_reduced = [[],[],[],[],[],[],[],]
+        # Step forward the environment
+        game.process()
 
-     # Reduces lists of possible orders to a single order
-     for player_ix, orders_per_player in enumerate(orders):
-        for order in orders_per_player:
-           # order is a list of possible orders
-           if len(order) > 1:
-              # Get legal one from list of possible actions - uses self.legal_actions
-              for possible_order in order:
-                 if possible_order in self.legal_actions[player_ix, :]:
-                    orders_reduced[player_ix].append(possible_order)
-                    break
-           else:
-              # Get string from single-item list
-              orders_reduced[player_ix].append(order[0])
-
-     orders = orders_reduced
-
-     # Set orders for each power
-     for power_ix, power_name in enumerate(power_names_sorted):
-        game.set_orders(power_name, orders[power_ix], expand=False)
-
-     # Step forward the environment
-     game.process()
-
-     # Store actions for next observation
-     self._last_actions = actions_per_player
+        # Store actions for next observation
+        self._last_actions = actions_per_player
