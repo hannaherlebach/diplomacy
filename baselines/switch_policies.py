3615b95e3fa2261af6190030f2a140121171af73
Hannah Erlebach <hannah.erlebach@gmail.com>
2023-08-19 18:11:47 +0100

    Unit adj vals only include enemy

--- baselines/switch_policies.py
+++ baselines/switch_policies.py
@@ -23,16 +23,18 @@ class PolicyUtils:
         self.map = game.map
 
 
-    def get_unit_adjacency_value(self, unit):
-        """Returns the number of units in adjacent provinces to the given unit.
+    def get_unit_adjacency_value(self, unit, unit_owner):
+        """Returns the number of enemy units in adjacent provinces to the given unit.
         
         Args:
-            unit: a MILA unit string."""
+            unit: a MILA unit string.
+            unit_owner: a Power instance"""
         
         loc = unit[2:]
         adj_locs = self.map.loc_abut[loc]
         adj_value = 0
-        for power in self.powers.values():
+        enemy_powers = [power for power in self.powers.values() if power != unit_owner]
+        for power in enemy_powers:
             for unit in power.units:
                 unit_loc = unit[2:]
                 adj_value += 1 if unit_loc in adj_locs else 0
@@ -189,7 +191,10 @@ class RandomDisbandPolicy:
         # In all other phases, hold
         
         return [actions, {'values': None, 'policy': None, 'actions': None}] #fill out this shit later
-
+    
+class SmartDisbandPolicy:
+    """Agent which disbands the units with fewest adjacent enemy units first."""
+    pass
 
 # Switch conditions
 def switch_after_k_turns(turn_num, power_ix, observation, k):
@@ -215,7 +220,6 @@ switch_after_10_supply_centers = partial(switch_after_k_supply_centers, k=10)
 switch_after_10_turns = partial(switch_after_k_turns, k=10)
 
 
-
 class SwitchPolicy:
     """Wrapper for network and hard-coded policies, with a switch when a given condition is met.
     
@@ -228,39 +232,47 @@ class SwitchPolicy:
         self.network_policy = network_policy # a network policy instance
         self.switch_condition = switch_condition
         self.hard_coded_policy = hard_coded_policy # a hard-coded policy instance
+        self.switch = [False for _ in range(7)]
         self.turn_num = 0
 
     def actions(self, slots_list, observation, legal_actions):
         
         print(f'{self.turn_num=}')
 
-        actions = []
+        # actions = []
 
-        # This doesn't quite work for switch_after_k_turns, not sure why
-        for power_ix in slots_list:
-            if not self.switch_condition(self.turn_num, power_ix, observation):
-                print('Network policy')
-                actions.append(self.network_policy.actions([power_ix], observation, legal_actions)[0][0])
-            else:
-                print('Hard-coded policy')
-                actions.append(self.hard_coded_policy.actions([power_ix], observation, legal_actions)[0][0])
+        # This will put actions in the wrong order
+
+        # unswitched = [power_ix for power_ix in slots_list if not self.switch_condition(self.turn_num, power_ix, observation)]
+        # switched = [power_ix for power_ix in slots_list if self.switch_condition(self.turn_num, power_ix, observation)]
+
+        # network_actions = self.network_policy.actions(unswitched, observation, legal_actions)
+        # hard_coded_actions = self.hard_coded_policy.actions(switched, observation, legal_actions)
+
+        # # This doesn't quite work for switch_after_k_turns, not sure why
+        # for power_ix in slots_list:
+        #     if not self.switch_condition(self.turn_num, power_ix, observation):
+        #         print('Network policy')
+        #         actions.append(self.network_policy.actions([power_ix], observation, legal_actions)[0][0])
+        #     else:
+        #         print('Hard-coded policy')
+        #         actions.append(self.hard_coded_policy.actions([power_ix], observation, legal_actions)[0][0])
+
+        actions = self.network_policy.actions(slots_list, observation, legal_actions) #tuple
+
+        for i, power_ix in enumerate(slots_list):
+            if self.switch_condition(self.turn_num, power_ix, observation) or self.switch[power_ix]:
+                self.switch[power_ix] = True
+                print('power', power_ix, 'disbanding from now on')
+                actions[0][i] = self.hard_coded_policy.actions([power_ix], observation, legal_actions)[0][0]
+                
+        # print(actions[0])
 
         self.turn_num += 1
 
-        return actions, {'values': None, 'policy': None, 'actions': None}
-    
-        # Version that assumes switch condition is same for all powers; this works for switch_after_k_turns
+        # print(len(actions[0]))
 
-        # if not self.switch_condition(self.turn_num, None, observation):
-        #     print('Network policy')
-        #     actions = self.network_policy.actions(slots_list, observation, legal_actions)
-        #     self.turn_num +=1
-        #     return actions
-        # else:
-        #     print('Hard-coded policy')
-        #     actions = self.hard_coded_policy.actions(slots_list, observation, legal_actions)
-        #     self.turn_num +=1
-        #     return actions
+        return actions # return values and policy for network policy
 
     def reset(self):
         self.network_policy.reset()
@@ -268,7 +280,44 @@ class SwitchPolicy:
 
 
 
-def run_experiment(policies, slots_to_policies, max_length=5):
+# Simultaneous Switch class deals with the case where all players switch at the same time more efficiently than Switch.
+
+def simultaneous_switch_after_k_turns(turn_num, observation, k):
+    """Returns True if turn_num > k, else False. Like switch_after_k_turns, but omits power_ix argument.
+    
+    How to use:
+        Create a partial function by specifying k, and pass to SwitchPolicy instance."""
+    return turn_num > k
+
+class SimultaneousSwitchPolicy:
+    """All players switch from the network policy to the hard-coded policy at the same time."""
+    def __init__(self, network_policy, switch_condition, hard_coded_policy):
+        self.network_policy = network_policy # a network policy instance
+        self.switch_condition = switch_condition # must be a property of the state, not individual powers
+        self.hard_coded_policy = hard_coded_policy # a hard-coded policy instance
+        self.turn_num = 0
+
+    def actions(self, slots_list, observation, legal_actions):
+        
+        print(f'{self.turn_num=}')
+
+        if not self.switch_condition(self.turn_num, observation):
+            print('Network policy')
+            actions = self.network_policy.actions(slots_list, observation, legal_actions)
+            self.turn_num +=1
+            return actions
+        else:
+            print('Hard-coded policy')
+            actions = self.hard_coded_policy.actions(slots_list, observation, legal_actions)
+            self.turn_num +=1
+            return actions
+
+    def reset(self):
+        self.network_policy.reset()
+        self.hard_coded_policy.reset()
+
+
+def run_experiment(policies, slots_to_policies, max_length=20):
     """Runs the chosen set of policies on Welfare Diplomacy."""
     game_instance = diplomacy_state.BaselineGame() # defo cursed
     initial_state = diplomacy_state.WelfareDiplomacyState(game_instance)
@@ -284,12 +333,20 @@ def run_experiment(policies, slots_to_policies, max_length=5):
 
 if __name__ == '__main__':
 
-    sl_policy = get_network_policy_instance()
+
     switch_after_10_turns = partial(switch_after_k_turns, k=10)
     switch_after_20_turns = partial(switch_after_k_turns, k=20)
-    random_disband_policy = RandomDisbandPolicy(num_players=7, p=0.5)
-    mixed_policy_1 = SwitchPolicy(sl_policy, switch_after_10_turns, random_disband_policy)
-    mixed_policy_2 = SwitchPolicy(sl_policy, switch_after_20_turns, random_disband_policy)
+    switch_after_5_supply_centers = partial(switch_after_k_supply_centers, k=5)
+
+
+    mixed_policy_1 = SwitchPolicy(get_network_policy_instance(), switch_after_10_turns, RandomDisbandPolicy(p=0.5))
+    mixed_policy_2 = SwitchPolicy(get_network_policy_instance(), switch_after_20_turns, RandomDisbandPolicy(p=0.5))
+    mixed_policy_3 = SwitchPolicy(get_network_policy_instance(), switch_after_5_supply_centers, RandomDisbandPolicy(p=0.5))
+
+    simultaneous_switch_after_10_turns = partial(simultaneous_switch_after_k_turns, k=10)
+    mixed_policy_4 = SimultaneousSwitchPolicy(get_network_policy_instance(), simultaneous_switch_after_10_turns, RandomDisbandPolicy(p=0.5))
+
+
 
 #     # Experiment 1
 #     print('Experiment 1')
@@ -301,7 +358,8 @@ if __name__ == '__main__':
 #     print('Experiment 2')
 #     trajectory = run_experiment(
 #         policies = (mixed_policy_1, mixed_policy_2),
-#         slots_to_policies = [0]*6 + [1]
+#         slots_to_policies = [0] * 6 + [1],
+#         max_length=30
 # )
 #     # Experiment 3
 #     print('Experiment 3')
@@ -323,8 +381,32 @@ if __name__ == '__main__':
 # )
 
     # Experiment 6
-    print('Experiment 6')
+#     print('Experiment 6')
+#     trajectory = run_experiment(
+#         policies = (mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2),
+#         slots_to_policies = list(range(7))
+# )
+
+    # Experiment 7
+    # print('Experiment 7')
+    # trajectory = run_experiment(
+    #     policies = (mixed_policy_1,),
+    #     slots_to_policies= [0] * 7,
+    #     max_length=20
+    # )
+    
+#     # Experiment 8
+#     print('Experiment 8')
+#     trajectory = run_experiment(
+#         policies = (mixed_policy_3,),
+#         slots_to_policies = [0] * 7,
+#         max_length=30
+# )
+
+    # Experiment 9
+    print('Experiment 9')
     trajectory = run_experiment(
-        policies = (mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2, mixed_policy_2),
-        slots_to_policies = list(range(7))
-)
+        policies = (mixed_policy_4,),
+        slots_to_policies = [0] * 7,
+        max_length=30
+    )
